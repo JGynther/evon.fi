2a592bfa9eea03a0d520c81ca8981bcd8e6c9417
infrapuna <78515417+infrapuna@users.noreply.github.com>
2021-08-15 03:55:18 +0300

    Added try catch

--- pages/api/submitform.js
+++ pages/api/submitform.js
@@ -20,47 +20,55 @@ async function main(data) {
       pass: process.env.EMAIL_PASS,
     }
   })
-
-  await transporter.sendMail({
-    from: "'Evon Group' <info@evon.fi>",
-    to: "joona.gynther@evon.fi, aatu.pulkkinen@evon.fi",
-    subject: `Uusi osakkeiden merkintä ${data.name}`,
-    html: 
-    ` <p>Nimi: ${data.name}</p>
-      <p>Sähköposti: <a href="mailto:${data.email}">${data.email}</a></p>
-      <p>Puhelinnumero: ${data.phone}</p>
-      <p>Lähiosoite: ${data.streetaddress}</p>
-      <p>Postinumero: ${data.postalcode}</p>
-      <p>Kaupunki: ${data.city}</p>
-      <p>Osakkeiden lukumäärä: ${data.stock}</p>`,
-  })
-
-  await transporter.sendMail({
-    from: "'Evon Group' <info@evon.fi>",
-    to: data.email,
-    replyTo: "aatu.pulkkinen@evon.fi",
-    subject: "Vahvistus osakkeiden merkinnästä - Evon Group",
-    html: `
-    <div>
-      <h3>Olet merkinnyt Evon Groupin osakkeita osakeannissamme.</h3>
-      <p>Alla vielä vahvistuksena tekemäsi merkintä. 
-      Mikäli esimerkiksi tietosi ovat väärin, ota yhteyttä mahdollisimman pian vastaamalla tähän sähköpostiin.</p>
-      <p>Kaikki merkinnät tarkistetaan vielä manuaalisesti. Otamme yhteyttä mikäli merkintäsi kanssa on ongelma.</p>
-      <br>
-      <div>
-        <p>Nimi: ${data.name}</p>
-        <p>Sähköposti: ${data.email}</p>
+  try {
+    await transporter.sendMail({
+      from: "'Evon Group' <info@evon.fi>",
+      to: "joona.gynther@evon.fi, aatu.pulkkinen@evon.fi",
+      subject: `Uusi osakkeiden merkintä ${data.name}`,
+      html: 
+      ` <p>Nimi: ${data.name}</p>
+        <p>Sähköposti: <a href="mailto:${data.email}">${data.email}</a></p>
         <p>Puhelinnumero: ${data.phone}</p>
         <p>Lähiosoite: ${data.streetaddress}</p>
         <p>Postinumero: ${data.postalcode}</p>
         <p>Kaupunki: ${data.city}</p>
-        <p>Osakkeiden lukumäärä: ${data.stock}</p>
-        <p>Merkintä euroina: ${data.stock * 0.3} EUR</p>
-      </div>
-    </div>
-    `
-  })
+        <p>Osakkeiden lukumäärä: ${data.stock}</p>`,
+    })
+  } catch(err) {
+    console.log(err)
+    return { error: true };
+  }
 
+  try {
+    await transporter.sendMail({
+      from: "'Evon Group' <info@evon.fi>",
+      to: data.email,
+      replyTo: "aatu.pulkkinen@evon.fi",
+      subject: "Vahvistus osakkeiden merkinnästä - Evon Group",
+      html: `
+      <div>
+        <h3>Olet merkinnyt Evon Groupin osakkeita osakeannissamme.</h3>
+        <p>Alla vielä vahvistuksena tekemäsi merkintä. 
+        Mikäli esimerkiksi tietosi ovat väärin, ota yhteyttä mahdollisimman pian vastaamalla tähän sähköpostiin.</p>
+        <p>Kaikki merkinnät tarkistetaan vielä manuaalisesti. Otamme yhteyttä mikäli merkintäsi kanssa on ongelma.</p>
+        <br>
+        <div>
+          <p>Nimi: ${data.name}</p>
+          <p>Sähköposti: ${data.email}</p>
+          <p>Puhelinnumero: ${data.phone}</p>
+          <p>Lähiosoite: ${data.streetaddress}</p>
+          <p>Postinumero: ${data.postalcode}</p>
+          <p>Kaupunki: ${data.city}</p>
+          <p>Osakkeiden lukumäärä: ${data.stock}</p>
+          <p>Merkintä euroina: ${data.stock * 0.3} EUR</p>
+        </div>
+      </div>
+      `
+    })
+  } catch(err) {
+    console.log(err)
+    return { error: true };
+  }
 }
 
 export default function handler(req, res) {
