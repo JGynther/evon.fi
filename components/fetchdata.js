1dc70da82136e7fd6418ed97d8e0f997fde44998
infrapuna <78515417+infrapuna@users.noreply.github.com>
2021-10-23 04:19:02 +0300

    Support for ranges infetchData

--- components/fetchdata.js
+++ components/fetchdata.js
@@ -1,7 +1,8 @@
 const axios = require('axios').default
 
 export default async function fetchData() {
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${process.env.GOOGLE_SHEETS_ID}/values/A1:J100?key=${process.env.GOOGLE_API_KEY}`
+    const ranges = ["C1:L100"]
+    let url = `https://sheets.googleapis.com/v4/spreadsheets/${process.env.GOOGLE_SHEETS_ID}/values/${ranges[0]}?key=${process.env.GOOGLE_API_KEY}`
     const data = await axios.get(url)
     return data.data.values.slice(1)
 }
