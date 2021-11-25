64032ef26ebe4268badbff4542d54d75cd46a87f
infrapuna <78515417+infrapuna@users.noreply.github.com>
2021-11-25 17:05:36 +0200

    Added more env exports

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
   env: {
     GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
     GOOGLE_SHEETS_ID: process.env.GOOGLE_SHEETS_ID,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_USERNAME: process.env.NEXTAUTH_USERNAME,
+    NEXTAUTH_PASSWORD: process.env.NEXTAUTH_PASSWORD,
   },
   reactStrictMode: true,
   async redirects() {
