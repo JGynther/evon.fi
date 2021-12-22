e3aebe9e01077030869efebc947d2da85767d212
infrapuna <78515417+infrapuna@users.noreply.github.com>
2021-12-22 17:26:20 +0200

    unescape newlines

--- pages/api/submit/6554.js
+++ pages/api/submit/6554.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
       const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEETS_6554_ID);
       await doc.useServiceAccountAuth({
         client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       });
       await doc.loadInfo();
       const sheet = doc.sheetsByIndex[0];
