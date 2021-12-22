55530726e782b71e1286cb81cf461153fec156d3
infrapuna <78515417+infrapuna@users.noreply.github.com>
2021-12-22 17:10:18 +0200

    Added error to api response

--- pages/api/submit/6554.js
+++ pages/api/submit/6554.js
@@ -28,8 +28,7 @@ export default async function handler(req, res) {
       });
       res.status(200).json();
     } catch (e) {
-      console.log(e);
-      res.status(500).json();
+      res.status(500).json(e);
     }
   }
 }
