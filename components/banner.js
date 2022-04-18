fa56e6aa8cd60c7d4e21920da569d1cdc4e982b1
Joona Gynther <joona@gynther.xyz>
2022-04-18 16:38:37 +0300

    Fixed banner

--- components/banner.js
+++ components/banner.js
@@ -1,19 +1,19 @@
-import { Title, Subtitle, Prose } from "@components/text";
+import { Title, Prose } from "@components/text";
+import Button from "@components/button";
 
-export default function Banner({
-  title,
-  long,
-  subtitle,
-  button,
-  href,
-  children,
-}) {
+export default function Banner({ title, long, button, href, children }) {
   return (
-    <div className="flex justify-center bg-indigo-500 bg-opacity-10  p-10">
-      <div>
-        {subtitle && <Subtitle>{subtitle}</Subtitle>}
+    <div className="flex justify-center bg-indigo-500 bg-opacity-10 p-10 rounded">
+      <div className="flex flex-col space-y-5">
         <Title long={long}>{title}</Title>
         <Prose>{children}</Prose>
+        {button && href && (
+          <span>
+            <Button type="link" href={href}>
+              {button}
+            </Button>
+          </span>
+        )}
       </div>
     </div>
   );
