d61508d700267caa4e6a7589ba5b3c3c8f47f8e5
Joona Gynther <joona@gynther.xyz>
2022-04-18 22:34:19 +0300

    Updated blogs

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -5,32 +5,20 @@ import Section from "@components/layout/section";
 import { Title, Subtitle, Prose } from "@components/text";
 
 export default function Post({ post }) {
-  const categories = post?.categories?.categories;
   const meta = <meta property="article:published" content={post.created_at} />;
   return (
     <Layout title={post.title} meta={meta}>
       <Section>
         <article>
-          {categories && (
-            <Subtitle>
-              <span className="flex flex-wrap gap-4">
-                {categories.map((category) => (
-                  <span key={category}>{category}</span>
-                ))}
-              </span>
-            </Subtitle>
-          )}
-          <Title noMargin>{post.title}</Title>
           <Subtitle>
             {new Date(post.created_at).toLocaleDateString()} by {post.author}
           </Subtitle>
-          <div>
-            {post.content.split("\n").map((line, index) => (
-              <Prose large key={index}>
-                {line}
-              </Prose>
-            ))}
-          </div>
+          <Title noMargin>{post.title}</Title>
+          {post.content.split("\n").map((line, index) => (
+            <Prose large key={index}>
+              {line}
+            </Prose>
+          ))}
         </article>
       </Section>
     </Layout>
