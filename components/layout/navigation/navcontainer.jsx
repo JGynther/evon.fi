ae0e04e233495b7c7fe95d2a7975d9806210efc9
Joona Gynther <joona@gynther.xyz>
2022-04-24 23:28:06 +0300

    Initial non-checked version of main page

--- components/layout/navigation/navcontainer.jsx
+++ components/layout/navigation/navcontainer.jsx
@@ -1,6 +1,6 @@
 export default function NavContainer({ children }) {
   return (
-    <div className="sticky top-0 z-10 mb-10">
+    <div className="sticky top-0 z-10 mb-12">
       <nav>
         <div className="bg-neutral-900 border-b border-neutral-700 divide-y divide-neutral-700">
           {children}
--- components/layout/section.js
+++ components/layout/section.js
@@ -1,6 +1,6 @@
 export default function Section({ id, children }) {
   return (
-    <section id={id || null} className="flex flex-col items-center mb-10">
+    <section id={id || null} className="flex flex-col items-center mb-20">
       <div className="w-full max-w-screen-md">{children}</div>
     </section>
   );
