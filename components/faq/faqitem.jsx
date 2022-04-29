b6764c2dfcc58e84f8f97689a4f878516d32788f
Joona Gynther <joona@gynther.xyz>
2022-04-29 19:59:53 +0300

    Updated FAQ

--- components/faq/faqitem.jsx
+++ components/faq/faqitem.jsx
@@ -3,7 +3,9 @@ import { Prose } from "@components/text";
 export default function FAQitem({ question, children }) {
   return (
     <details className="mt-10">
-      <summary className="text-xl">{question}</summary>
+      <summary className="text-white text-opacity-80 text-xl">
+        {question}
+      </summary>
       <Prose large>{children}</Prose>
     </details>
   );
