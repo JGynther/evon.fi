9e8c5dd5b3dd64c958843dfecffabdff7fa732f8
Joona Gynther <joona@gynther.xyz>
2022-04-30 19:15:14 +0300

    Update input to show required status

--- components/formcontrol/index.jsx
+++ components/formcontrol/index.jsx
@@ -1,10 +1,22 @@
-export function Input({ label, type, error, setError, onChange, ...rest }) {
+export function Input({
+  label,
+  type,
+  required,
+  error,
+  setError,
+  onChange,
+  ...rest
+}) {
   return (
     <div className="flex flex-col gap-3">
-      <label className="text-white tracking-wide text-lg">{label}</label>
+      <label className="text-white tracking-wide text-lg">
+        {label}
+        {required ? <span className="text-rose-500"> *</span> : null}
+      </label>
       <input
         type={type}
         onChange={onChange}
+        required={required}
         {...rest}
         className="bg-neutral-900 rounded px-4 py-2 border border-neutral-700 hover:border-neutral-500 tranform hover:-translate-y-1 focus:-translate-y-1 shadow hover:shadow-lg focus:shadow-lg outline-none focus:ring-1 focus:border-indigo-700 transition"
       />
