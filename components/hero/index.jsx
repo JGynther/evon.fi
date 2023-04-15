3bad77efe4e60f46b53cf6fe406ce088c9fcdc59
Joona Gynther <joona@gynther.xyz>
2023-04-15 23:22:35 +0300

    update hero for new osakeanti link

--- components/hero/index.jsx
+++ components/hero/index.jsx
@@ -4,37 +4,37 @@ import Button from "@components/button";
 import Link from "next/link";
 
 export default function Hero() {
-  return (
-    <Section>
-      <div className="text-center space-y-8 md:my-14">
-        <Title>
-          <span className="text-4xl md:text-6xl">
-            Ole opportunisti, älä pelkkä sijoittaja
-          </span>
-        </Title>
-        <div className="text-white text-opacity-80 text-lg">
-          Evon Capital on tuottohakuisten piensijoittajien yritys, joka
-          sijoittaa varojaan pitkällä aikahorisontilla. Yhtiö yhdistää niin
-          osakkaidensa varat kuin osaamisen — ja hakee markkinoihin nähden
-          tuottoa tällä synergialla.
-        </div>
-        <div className="text-white text-opacity-80 text-lg">
-          Kiinnostaako lähteä mukaan? 👇
-        </div>
-        <div className="">
-          <Button type="link" href="/waitlist">
-            Liity odotuslistalle
-          </Button>
-        </div>
-        <div>
-          <Link
-            href="/about"
-            className="text-white text-opacity-60 hover:text-opacity-80 transition underline text-sm"
-          >
-            tai lue lisää
-          </Link>
-        </div>
-      </div>
-    </Section>
-  );
+	return (
+		<Section>
+			<div className="text-center space-y-8 md:my-14">
+				<Title>
+					<span className="text-4xl md:text-6xl">
+						Ole opportunisti, älä pelkkä sijoittaja
+					</span>
+				</Title>
+				<div className="text-white text-opacity-80 text-lg">
+					Evon Capital on tuottohakuisten piensijoittajien yritys, joka
+					sijoittaa varojaan pitkällä aikahorisontilla. Yhtiö yhdistää niin
+					osakkaidensa varat kuin osaamisen — ja hakee markkinoihin nähden
+					tuottoa tällä synergialla.
+				</div>
+				<div className="text-white text-opacity-80 text-lg">
+					Kiinnostaako lähteä mukaan? 👇
+				</div>
+				<div className="">
+					<Button type="link" href="https://osakeanti.evon.fi/anti/A-1-2023">
+						Osallistu osakeantiin
+					</Button>
+				</div>
+				<div>
+					<Link
+						href="/about"
+						className="text-white text-opacity-60 hover:text-opacity-80 transition underline text-sm"
+					>
+						tai lue lisää
+					</Link>
+				</div>
+			</div>
+		</Section>
+	);
 }
